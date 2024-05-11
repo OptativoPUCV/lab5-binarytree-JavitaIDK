f9ac16ec4ce6b38bff5a5edd5602a6debe2e26e2
Your Name <you@example.com>
2024-05-11 22:47:59 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -47,10 +47,42 @@ TreeMap * createTreeMap(int (*lower_than) (void* key1, void* key2)) {
 
 
 void insertTreeMap(TreeMap * tree, void* key, void * value) {
-  if(tree == NULL || key == NULL || value == NULL) return;
-  
-  
-  return;
+  if(tree == NULL)return;
+  if(tree->root == NULL) //si el arbol esta vacio, el nuevonodo se convertira en la raiz
+  {
+    tree->root = createTreeNode(key,value);
+    return;
+  }
+  TreeNode *currentAUX = tree->root; //nodo actual
+  while(currentAUX != NULL)
+    {
+      if(is_equal(tree,key,currentAUX->pair->key))return; 
+      //PARA INSERTAR A LA IZQUIERDA
+      if(tree->lower_than(key,currentAUX->pair->key)) //si la clave es menor que el current, se va a la izq
+      {
+        if(currentAUX->left == NULL) //si no hay nodo a la izq se crea uno y se asigna como hijo
+        {
+          TreeNode *newNodo = createTreeNode(key,value);
+          newNodo->parent = currentAUX;
+          currentAUX->left = newNodo;
+          tree->current = newNodo; //actualiza nodo
+          return;
+        }
+        if(currentAUX->left != NULL)currentAUX = currentAUX->left;
+      }//PA INSERTAR A LA DERECHA  
+      if(tree->lower_than(currentAUX->pair->key,key))
+      {
+        if(currentAUX->right == NULL)
+        {
+          TreeNode *newNodo = createTreeNode(key,value);
+          newNodo->parent = currentAUX;
+          currentAUX->right = newNodo;
+          tree->current = newNodo; //actualiza nodo
+          return;
+        }
+        if(currentAUX->right != NULL) currentAUX = currentAUX->right; //si hay nodo derecho, avanza a la derecha
+      }
+    }
   
 
 }
